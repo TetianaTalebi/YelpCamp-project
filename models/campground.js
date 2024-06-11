bb1b145c72ab4f8df6de2084af5f59dc8d5f39cb
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-06-10 22:40:29 -0400

    Create our Campground Schema and Campground Model

---
+++ models/campground.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+// Making a shortcut for mongoose.Schema
+const Schema = mongoose.Schema;
+
+const CampgroundSchema = new Schema({
+    title: String,
+    price: Number,
+    description: String,
+    location: String
+});
+
+// Exporting our Campground model
+module.exports = mongoose.model('Campground', CampgroundSchema);
