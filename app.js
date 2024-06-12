8733726e337d1e0683aaf5ce93e5aa11656c5d81
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-06-11 21:44:19 -0400

    Add mongoose connection event listeners to app.js

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const path = require('path');
 const mongoose = require('mongoose');
+
+mongoose.connection.on('connected', () => console.log('connected'));
+mongoose.connection.on('open', () => console.log('open'));
+mongoose.connection.on('disconnected', () => console.log('disconnected'));
+mongoose.connection.on('reconnected', () => console.log('reconnected'));
+mongoose.connection.on('disconnecting', () => console.log('disconnecting'));
+mongoose.connection.on('close', () => console.log('close'));
+
 mongoose.connect('mongodb://127.0.0.1:27017/myyelpcamp')
 .then(() => console.log("Mongo connection is open!"))
 .catch((err) => {
