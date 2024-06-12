ad338c9c883751139891c1ca979576451385cd38
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-06-11 22:47:38 -0400

    Add db connection settings and db connection event listeners into index.js

---
+++ seeds/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const db = mongoose.connection;
+
+db.on('connected', () => console.log('connected'));
+db.on('open', () => console.log('open'));
+db.on('disconnected', () => console.log('disconnected'));
+db.on('reconnected', () => console.log('reconnected'));
+db.on('disconnecting', () => console.log('disconnecting'));
+db.on('close', () => console.log('close'));
+
+mongoose.connect('mongodb://127.0.0.1:27017/myyelpcamp')
+.then(() => console.log("Mongo connection is open!"))
+.catch((err) => {
+    console.log("Mongo connection error!");
+    console.log(err)
+})
