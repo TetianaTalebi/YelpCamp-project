d75f50ae49ef78674795e002aefccb01017a32ab
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-06-16 22:30:08 -0400

    Add an internal code into route app.post(/campgrounds... in app.js

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// A middleware for parsing req.body
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
@@ -50,8 +51,12 @@ app.get('/campgrounds/new', (req, res) => {
 })
 
 app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+
     // res.send(req.body)
-    console.log(req.body.campground)
+    // console.log(req.body.campground)
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
