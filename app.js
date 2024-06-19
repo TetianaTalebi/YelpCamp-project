1a02b7981b2e4ec7601c1987a6a5c82ce62ff57c
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-06-18 20:01:55 -0400

    Require method-override in app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 // Require Campground model from models/campground.js
 const Campground = require('./models/campground');
