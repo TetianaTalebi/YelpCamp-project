15e5b5a91510b4fdb5186fd8acbbcc30da3c94de
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-06-25 22:13:27 -0400

    Require ejs-mate middleware into app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 
 // Require Campground model from models/campground.js
