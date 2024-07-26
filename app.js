d9d24bb6950bcaf15753f883cafb63d0096a8731
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-07-25 23:00:40 -0400

    Wrap the async route handler in the route app.delete(/campgrounds/:id... with catchAsync function

--- app.js
+++ app.js
@@ -88,11 +88,11 @@ app.put('/campgrounds/:id', catchAsync(async (req, res) => {
     // res.send('It works!!!')
 }))
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds')
-})
+}))
 
 // app.get('/makecampground', async (req, res) => {
 //     const camp = new Campground({title: 'My Backyard', description: 'Cheap Camping'});
