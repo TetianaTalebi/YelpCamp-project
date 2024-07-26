8acdcb049f4aec6f13e4e1cd2bcc6b30c12b9ac9
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-07-25 22:58:48 -0400

    Wrap the route handler in the route app.put(/campgrounds/:id... with catchAsync function

--- app.js
+++ app.js
@@ -80,13 +80,13 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/edit', {campground})
 }))
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
     const {id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id, req.body.campground);
     res.redirect(`/campgrounds/${campground._id}`);
     // console.log(req.body.campground);
     // res.send('It works!!!')
-})
+}))
 
 app.delete('/campgrounds/:id', async (req, res) => {
     const {id} = req.params;
