890f379f85d83e774619a4f34f73e68f7f12c9bf
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-08-01 22:57:58 -0400

    Move campgroundSchema from app.js into schemas.js

--- app.js
+++ app.js
@@ -47,17 +47,6 @@ app.use(methodOverride('_method')); // '_method' will be used as a query string
 
 const validateCampground = (req, res, next) => {
 
-    const campgroundSchema = Joi.object({
-        campground: Joi.object(
-            {
-                title: Joi.string().required(),
-                image: Joi.string().required(),
-                price: Joi.number().required().min(0),
-                description: Joi.string().required(),
-                location: Joi.string().required()
-            }
-        ).required()
-    });
     const {error} = campgroundSchema.validate(req.body);
 
     if (error){
