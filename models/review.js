a44de6219035575cc00b22b47360dc53eb6a00af
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-08-19 00:00:36 -0400

    Define a review Schema in models/review.js

--- models/review.js
+++ models/review.js
@@ -2,3 +2,11 @@ const mongoose = require('mongoose');
 
 // Defining a shortcut for mongoose.Schema
 const Schema = mongoose.Schema;
+
+// Defining a review Mongoose Schema
+const ReviewSchema = new Schema({
+    body: String,
+    rating: Number
+});
+
+
