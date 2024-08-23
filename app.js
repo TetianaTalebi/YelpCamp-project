d92ae34f2e008da007494d855aeebec2e0d085cf
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-08-22 23:10:20 -0400

    Populate a campground found by id with reviews inside a callback for route app.get(/campgrounds/:id... in app.js

--- app.js
+++ app.js
@@ -95,8 +95,8 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
 
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const {id} = req.params;
-    const campground = await Campground.findById(id);
-    // console.log(id)
+    const campground = await Campground.findById(id).populate('reviews');
+    // console.log(campground)
     res.render('campgrounds/show', { campground })
 }))
 
