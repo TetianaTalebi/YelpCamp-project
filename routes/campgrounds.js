afcb65ee964d4b7afdc417b1ee2624b06cd94c92
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-09-04 23:28:21 -0400

    Add comments that describe campground route functions into routes/campgrounds.js

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+// A route for viewing all campgrounds
 router.get('/campgrounds', catchAsync(async (req, res) => {
 
     // Find all campgrounds in db and save it to const campgrounds
@@ -10,10 +11,12 @@ router.get('/campgrounds', catchAsync(async (req, res) => {
     res.render('campgrounds/index', {campgrounds});
 }))
 
+// A route that renders a form for creating a new campground
 router.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
 
+// A route for posting a new campground into a database
 router.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
     
     const campground = new Campground(req.body.campground);
@@ -21,6 +24,7 @@ router.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
+// A route for viewing a particular campground
 router.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const {id} = req.params;
     const campground = await Campground.findById(id).populate('reviews');
@@ -28,11 +32,13 @@ router.get('/campgrounds/:id', catchAsync(async (req, res) => {
     res.render('campgrounds/show', { campground })
 }))
 
+// A route that renders a form for editing a particular campground
 router.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', {campground})
 }))
 
+// A route that posts an update for a particular campground into a database
 router.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
 
     const {id} = req.params;
@@ -41,6 +47,7 @@ router.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) =
     
 }))
 
+// A route that deletes a particular campground from a database
 router.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
