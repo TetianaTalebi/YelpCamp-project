9bde0f191f6e0f92ad5d01d2892e079bc919c0da
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-09-05 23:41:52 -0400

    Delete campgrounds/:id/reviews prefix from review routes in routes/reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Creating a nested route for adding reviews for a campground
-router.post('/campgrounds/:id/reviews', validateReview, catchAsync( async (req, res) => {
+router.post('/', validateReview, catchAsync( async (req, res) => {
     const campground = await Campground.findById(req.params.id);
 
     // Instantiating a new review
@@ -16,7 +16,7 @@ router.post('/campgrounds/:id/reviews', validateReview, catchAsync( async (req, 
 }))
 
 // Adding a delete route for campground reviews
-router.delete('/campgrounds/:id/reviews/:reviewId', catchAsync( async (req, res) => {
+router.delete('/:reviewId', catchAsync( async (req, res) => {
     const {id, reviewId} = req.params;
     await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
     await Review.findByIdAndDelete(reviewId);
