76022e6b051d4cc43a936e28f83ab47bf1142d4b
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-09-21 00:30:38 -0400

    Define a UserSchema in models/user.js, pass passportLocalMongoose to UserSchema.plugin, compile a User Model and export it

--- models/user.js
+++ models/user.js
@@ -4,3 +4,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+
+        // This is not considered as a validation
+        // The validation middleware will be created separately
+        unique: true
+    }
+});
+
+// This adds on a username, a hash and salt fields to a UserSchema,
+// makes sure that the usernames are unique and not duplicated
+// Also it adds on some more additional methods
+UserSchema.plugin(passportLocalMongoose);
+
+// Compile and export a User Model
+module.exports = mongoose.model('User', UserSchema);
