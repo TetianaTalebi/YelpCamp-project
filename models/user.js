b18a6cec00a3c78d4f75da40e8720b2d51baa1cb
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-09-21 00:16:21 -0400

    Require Passport-Local-Mongoose into models/user.js

--- models/user.js
+++ models/user.js
@@ -2,3 +2,5 @@ const mongoose = require('mongoose');
 
 // Create a shortcut for mongoose.Schema
 const Schema = mongoose.Schema;
+
+const passportLocalMongoose = require('passport-local-mongoose');
