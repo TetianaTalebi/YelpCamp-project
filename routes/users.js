f6478ea3a7fbf13bc367506a624c0ce01667ed25
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-09-22 23:33:48 -0400

    Create a route router.post(/register... in routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,13 @@ router.get('/register', (req, res)=>{
     res.render('users/register');
 })
 
+router.post('/register', async (req, res) =>{
+    const {email, username, password} = req.body;
+    const user = new User({email, username});
+    const registeredUser = await User.register(user, password);
+    console.log(registeredUser);
+    req.flash('success', 'Welcome to YelpCamp!');
+    res.redirect('/campgrounds');
+})
+
 module.exports = router;
