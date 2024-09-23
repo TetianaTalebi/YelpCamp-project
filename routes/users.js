ec721cd3514e18fae80becf534c46ccb12ed81c1
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-09-22 22:31:11 -0400

    Create a route in routes/users.js that gives a registration form for a user

--- routes/users.js
+++ routes/users.js
@@ -2,4 +2,9 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
+// This route gives a registration form for a user
+router.get('/register', (req, res)=>{
+    res.render('users/register');
+})
+
 module.exports = router;
