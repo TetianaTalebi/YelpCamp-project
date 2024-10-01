14e42ad21c2ef60a887fab8568b57b9dfd65c9dc
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-10-01 01:02:58 -0400

    Move a route handler for router.post(/register... into a controller users.register

--- controllers/users.js
+++ controllers/users.js
@@ -5,3 +5,25 @@ const User = require('../models/user');
 module.exports.renderRegister = (req, res)=>{
     res.render('users/register');
 }
+
+// Registers a user
+module.exports.register = async (req, res, next) =>{
+    // In order a user has much better experience, 
+    // 'try catch' is used to flash error messages
+    // Passport-Local Node package makes a basic validation and provides error messages
+    try {
+        const {email, username, password} = req.body;
+        const user = new User({email, username});
+        const registeredUser = await User.register(user, password);
+
+        // This code logs a user in
+        req.login(registeredUser, function (err){
+            if(err) { return next(err); }
+            req.flash('success', 'Welcome to YelpCamp!');
+            res.redirect('/campgrounds'); 
+        });  
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('/register');
+    }
+}
--- routes/users.js
+++ routes/users.js
@@ -9,26 +9,8 @@ const { storeReturnTo } = require('../middleware');
 // This route gives a registration form for a user
 router.get('/register', users.renderRegister);
 
-router.post('/register', catchAsync( async (req, res, next) =>{
-    // In order a user has much better experience, 
-    // 'try catch' is used to flash error messages
-    // Passport-Local Node package makes a basic validation and provides error messages
-    try {
-        const {email, username, password} = req.body;
-        const user = new User({email, username});
-        const registeredUser = await User.register(user, password);
-
-        // This code logs a user in
-        req.login(registeredUser, function (err){
-            if(err) { return next(err); }
-            req.flash('success', 'Welcome to YelpCamp!');
-            res.redirect('/campgrounds'); 
-        });  
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('/register');
-    }
-}))
+// This route registers a user
+router.post('/register', catchAsync(users.register));
 
 router.get('/login', (req, res)=>{
     res.render('users/login');
