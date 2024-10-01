25bb02813a9c5997d376eec8dee4857268bb3f54
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-10-01 00:33:00 -0400

    Require reviews controllers into routes/reviews.js and move a route handler for router.post(/... into a controller reviews.createReview

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,3 +1,18 @@
 // Require Campground and Review Models
 const Campground = require('../models/campground');
 const Review = require('../models/review');
+
+// Creates a review for a campground
+module.exports.createReview = async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+
+    // Instantiating a new review
+    const review = new Review(req.body.review);
+    review.author = req.user._id;
+   
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    req.flash('success', 'Created new review!!!')
+    res.redirect(`/campgrounds/${campground._id}`);
+}
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
 
+// Require reviews controllers
+const reviews = require('../controllers/reviews');
+
 // Require Campground model from models/campground.js
 const Campground = require('../models/campground');
 
@@ -12,19 +15,7 @@ const catchAsync = require('../utils/catchasync');
 const {validateReview, isLoggedIn, isReviewAuthor} = require('../middleware');
 
 // Creating a nested route for adding reviews for a campground
-router.post('/', isLoggedIn, validateReview, catchAsync( async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-
-    // Instantiating a new review
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-   
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success', 'Created new review!!!')
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
 // Adding a delete route for campground reviews
 router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync( async (req, res) => {
