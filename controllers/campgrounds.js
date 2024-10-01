a4d148679d5ee854244273a5ab262e7c215e3f38
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-09-30 23:23:31 -0400

    Add comments to a function index in controllers/campgrounds.js

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -2,6 +2,8 @@ const Campground = require('../models/campground');
 
 // This 'index' function finds all campgrounds and renders index page
 module.exports.index = async (req, res) => {
+    // Find all campgrounds in db and save it to const campgrounds
     const campgrounds = await Campground.find({});
+    // Pass the found data to our template
     res.render('campgrounds/index', {campgrounds});
 }
