73b8a21ef851c60a633a3bb3d86ccdba340a0eb9
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-10-01 22:16:26 -0400

    Group routes that have similar paths together in routes/campgrounds.js

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -8,28 +8,24 @@ const {isLoggedIn, validateCampground, isAuthor} = require('../middleware');
 
 const catchAsync = require('../utils/catchasync');
 
-// Require Campground model from models/campground.js
-const Campground = require('../models/campground');
-
+router.route('/')
 // A route for viewing all campgrounds
-router.get('/', catchAsync(campgrounds.index));
+.get(catchAsync(campgrounds.index))
+// A route for posting a new campground into a database
+.post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
 // A route that renders a form for creating a new campground
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-// A route for posting a new campground into a database
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
-// A route for viewing a particular campground
-router.get('/:id', catchAsync(campgrounds.showCampground));
-
 // A route that renders a form for editing a particular campground
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
+router.route('/:id')
+// A route for viewing a particular campground
+.get(catchAsync(campgrounds.showCampground))
 // A route that posts an update for a particular campground into a database
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
+.put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
 // A route that deletes a particular campground from a database
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+.delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
