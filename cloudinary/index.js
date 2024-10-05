887d824369456d00c075a1496ce5eaf1f7d0259e
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-10-04 22:45:43 -0400

    Add cloudinary configuration into cloudinary/index.js

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,2 +1,10 @@
 const cloudinary = require('cloudinary').v2;
 const {CloudinaryStorage} = require('multer-storage-cloudinary');
+
+// Configuration
+// This code associates our Cloudinary account with this 'cloudinary' object
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_CLOUD_KEY,
+    api_secret: process.env.CLOUDINARY_CLOUD_SECRET
+});
