1e385823c7246058b085a5813f4ca7a2c03a27a9
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-10-24 01:58:02 -0400

    Remove console.log(e.features[0]) from public/javascripts/clusterMap.js

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -93,7 +93,6 @@ map.on('load', function () {
     // the location of the feature, with
     // description HTML from its properties.
     map.on('click', 'unclustered-point', function (e) {
-        console.log(e.features[0]);
         const { popUpMarkup } = e.features[0].properties;
         const coordinates = e.features[0].geometry.coordinates.slice();
 
