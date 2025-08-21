e895224e7c19b8f8ca1afb194f164f50fd667e6b
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2025-08-21 17:31:12 -0400

    Save the changes

--- seeds/index.js
+++ seeds/index.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV!=='production'){
+    require('dotenv').config();
+}
+
 const mongoose = require('mongoose');
 
 const db = mongoose.connection;
@@ -14,7 +18,13 @@ db.on('reconnected', () => console.log('reconnected'));
 db.on('disconnecting', () => console.log('disconnecting'));
 db.on('close', () => console.log('close'));
 
-mongoose.connect('mongodb://127.0.0.1:27017/myyelpcamp')
+// Production database
+// const dbUrl = process.env.DB_URL;
+
+//local database
+const dbUrl = 'mongodb://127.0.0.1:27017/myyelpcamp';
+
+mongoose.connect(dbUrl)
 .then(() => console.log("Mongo connection is open!"))
 .catch((err) => {
     console.log("Mongo connection error!");
@@ -26,8 +36,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/myyelpcamp')
 const sample = array => array[Math.floor(Math.random()*array.length)];
 
 const price = Math.floor(Math.random()*20)+19;
-
-
 
 const seedDB = async () => {
 
@@ -48,16 +56,16 @@ const seedDB = async () => {
             title: `${sample(descriptors)} ${sample(places)}`,
             images: [
                 {
-                    url:'https://res.cloudinary.com/dljmlxtmx/image/upload/v1728274651/YelpCamp/ldjs8sxrmnsxy0jbrpzc.jpg',
+                    url:'https://res.cloudinary.com/dljmlxtmx/image/upload/v1728789755/YelpCamp/y8n7kyigm3atucteitay.jpg',
                     filename: 'YelpCamp/ldjs8sxrmnsxy0jbrpzc'
                 },{
-                    url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1728274651/YelpCamp/rglqq3sjus1rszypyxqt.jpg',
+                    url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1728700713/YelpCamp/gfkmkfkwxkxo9umiu2te.jpg',
                     filename: 'YelpCamp/rglqq3sjus1rszypyxqt'
                 },{
-                    url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1728274651/YelpCamp/ektmez6kxmfv2huod6sz.jpg',
+                    url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1728700713/YelpCamp/h9ew8qvx7djmvjr6qvdj.jpg',
                     filename: 'YelpCamp/ektmez6kxmfv2huod6sz'
                 },{
-                    url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1728274651/YelpCamp/a2erd1lfnq9naz8atwbo.jpg',
+                    url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1728789755/YelpCamp/jji26dp2xxjfrskslzew.jpg',
                     filename: 'YelpCamp/a2erd1lfnq9naz8atwbo'
                 }
             ],
@@ -71,6 +79,12 @@ const seedDB = async () => {
             price,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis saepe nulla rerum, sit perferendis cumque eos labore minima esse blanditiis assumenda maiores animi non commodi voluptatem eveniet eum quas molestiae?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae velit odit libero facere soluta, magnam molestias rerum aspernatur perferendis possimus dolores voluptate repudiandae tempora provident incidunt cumque repellendus in adipisci. Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
             author: '66f0e0cfafa6e339ae846860',
+
+            // author for external database
+            // '672658a117ef894175db3f95'
+
+            // author for internal database
+            // '66f0e0cfafa6e339ae846860'
             location: `${cities[randomCities].city}, ${cities[randomCities].state}`
 
         });
