0c5fae069f9392f77ab454ac71abfe0ccd80ad7e
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2025-09-18 22:46:38 -0400

    Define let totalQtyCamps in public/javascripts/pagination.js

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,3 +1,6 @@
 // The total number of campgrounds at one 'All Campgrounds' page
 let campsPerPage = 10;
 
+// The total qu-ty of campgrounds that app has
+// totalCamps was defined in campgrounds/index.ejs template
+let totalQtyCamps = totalCamps;
