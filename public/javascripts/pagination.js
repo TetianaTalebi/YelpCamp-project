bf889ed69bf2d268829299c4c327f13e2e234d67
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2025-09-23 16:29:02 -0400

    Add toggleHiddenClass function

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -14,3 +14,23 @@ function findActivePageNumber(){
     const curPageLiActive = document.querySelector('.page.active');
     return Number(curPageLiActive.children[0].innerText);
 }
+
+// toggleHiddenClass function accepts the number of the currently active page and toggle .hidden class of the campground cards (to show only that cards that relate to the currently active page)
+
+function toggleHiddenClass(ActivePageNum){
+    // The first element in the collection of elements (i.e. campground cards) has index 0
+    // Define index of the first visible element (campground card) at the current page
+    let firstElOfCurPageIndex = (ActivePageNum-1)*campsPerPage;
+
+    // Grab all el-s from paginated container
+    const allPaginatedEls = document.querySelectorAll('#paginated > .campground-card');
+
+    for (let i = 0; i<campsPerPage; i++) {
+        // if an element exists (i.e. is not undefined) toggle its class .hidden
+        if(allPaginatedEls[firstElOfCurPageIndex + i]){
+        allPaginatedEls[firstElOfCurPageIndex + i].classList.toggle('hidden');
+        }
+    }
+}
+
+toggleHiddenClass(1);
