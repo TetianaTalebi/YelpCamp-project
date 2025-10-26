9be789d038d121405cfbd9988519d6684efa1560
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2025-10-26 00:03:28 -0400

    Add styles for #showMapBtn and #showPriceBtn

--- public/javascripts/show.js
+++ public/javascripts/show.js
@@ -2,6 +2,10 @@ const showMapBtn = document.querySelector('#showMapBtn');
 
 const showPriceBtn = document.querySelector('#showPriceBtn');
 
+const showCampDescription = document.querySelector('#showCampDescription');
+let showCampDescrText = showCampDescription.innerText;
+// console.log(showCampDescrText);
+
 function clickOnShowMap() {
     const showHideMap = document.querySelector('#showHideMap');
     showHideMap.classList.remove('hidden');
@@ -13,6 +17,14 @@ function clickOnShowPrice(){
     showHideCampPriceTaxes.classList.remove('hidden');
     showPriceBtn.classList.add('hidden');
 }
+
+function displayCampDescription(){
+    if (showCampDescription.classList.contains('showCampDescrShort')){
+        showCampDescription.innerText = showCampDescrText.slice(0, 475) + '...';  
+    }
+}
+
+window.addEventListener('load', displayCampDescription);
 
 showMapBtn.addEventListener('click', clickOnShowMap);
 
