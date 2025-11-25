bdcaeedb906476abc88b9016115bac726bf6f17c
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2025-11-25 14:52:55 -0500

    Clean and reorder code in app.js

--- app.js
+++ app.js
@@ -1,34 +1,20 @@
-if(process.env.NODE_ENV!=='production'){
+if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config();
 }
-// process.env.NODE_ENV='production';
-// require('dotenv').config();
 
 const express = require('express');
-
 const path = require('path');
 const mongoose = require('mongoose');
-
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const User = require('./models/user');
-
-const helmet = require('helmet');
-
-const mongoSanitize = require('express-mongo-sanitize');
-
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
-
-const MongoStore = require('connect-mongo');
-
-const mongoStoreSecret = process.env.MONGO_STORE_PW;
-
-const sessionConfigSecret = process.env.SESSION_CONFIG_SECRET;
-
 const flash = require('connect-flash');
 const ExpressError = require('./utils/expresserror');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
 
 // Require user routes
 const userRoutes = require('./routes/users');
@@ -39,25 +25,10 @@ const campgroundRoutes = require('./routes/campgrounds');
 // Require review routes
 const reviewRoutes = require('./routes/reviews');
 
-// Local database
-// const dbUrl = 'mongodb://127.0.0.1:27017/myyelpcamp';
+const MongoStore = require('connect-mongo');
 
-// Production database
-const dbUrl = process.env.DB_URL;
-
-const store = MongoStore.create({
-    mongoUrl: dbUrl,
-
-    //  A session will be updated one time a day
-    touchAfter: 24 * 60 * 60, // Time period in seconds.
-    crypto: {
-        secret: mongoStoreSecret
-    }
-});
-
-store.on('error', function(e){
-    console.log("Session store error!", e);
-});
+// Production and local databases
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/myyelpcamp';
 
 // Create a mongoose.connection shortcut
 const db = mongoose.connection;
@@ -73,27 +44,24 @@ mongoose.connect(dbUrl)
 .then(() => console.log("Mongo connection is open!"))
 .catch((err) => {
     console.log("Mongo connection error!");
-    console.log(err)
-})
+    console.log(err);
+});
 
 const app = express();
 
-// We are telling Express to use ejs-mate to parse ejs
-// instead of default ejs engine
+// We are telling Express to use ejs-mate to parse ejs instead of default ejs engine
 app.engine('ejs', ejsMate);
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // A middleware for parsing req.body
 app.use(express.urlencoded({extended: true}));
 
-// Load 'method-override' middleware
-// that lets us use HTTP verbs such as PUT or DELETE 
-// in places where the client doesn't support it.
-app.use(methodOverride('_method')); // '_method' will be used as a query string
+// 'method-override' middleware lets us use HTTP verbs such as PUT or DELETE in places where the client doesn't support them
+// '_method' will be used as a query string
+app.use(methodOverride('_method')); 
 
-// Telling Express to serve 'public' directory
+// Tell Express to serve 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Replace the prohibited characters '$' and '.' with '_' in
@@ -106,10 +74,43 @@ app.use(mongoSanitize({
     }),
 );
 
-// Telling Express to use 'Connect-Flash' Node package
+const mongoStoreSecret = process.env.MONGO_STORE_PW;
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+
+    //  A session will be updated one time a day
+    touchAfter: 24 * 60 * 60, // Time period in seconds.
+    crypto: {
+        secret: mongoStoreSecret
+    }
+});
+
+store.on('error', function(e){
+    console.log("Session store error!", e);
+});
+
+const sessionConfigSecret = process.env.SESSION_CONFIG_SECRET;
+
+const sessionConfig = {
+    store, // it's equal to 'store: store'
+    name: '_y_c',
+    secret: sessionConfigSecret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { 
+        httpOnly: true,
+        expires: Date.now()+1000*60*60*24*7,
+        maxAge: 7*24*60*60*1000,
+    }
+};
+
+app.use(session(sessionConfig));
+
+// Tell Express to use 'Connect-Flash' Node package
 app.use(flash());
 
-// Telling Express to use 'Helmet' Node package
+// Tell Express to use 'Helmet' Node package
 app.use(helmet());
 
 const scriptSrcUrls = [
@@ -158,20 +159,8 @@ app.use(
     })
 );
 
-const sessionConfig = {
-    store, // it's equal to 'store: store'
-    name: '_y_c',
-    secret: sessionConfigSecret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { 
-        httpOnly: true,
-        expires: Date.now()+1000*60*60*24*7,
-        maxAge: 7*24*60*60*1000,
-    }
-};
 
-app.use(session(sessionConfig));
+
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -179,8 +168,7 @@ app.use(passport.session());
 // Tell passport to use a LocalStrategy
 // The authenticate method for LocalStrategy locates on a User Model
 
-// authenticate(), serializeUser(), deserializeUser() are methods on a User Model 
-// that were added automatically by a plugin passportLocalMongoose
+// authenticate(), serializeUser(), deserializeUser() are methods on a User Model that were added automatically by a plugin passportLocalMongoose
 passport.use(new LocalStrategy(User.authenticate()));
 
 // This refers to how a user is stored on a session
@@ -189,25 +177,14 @@ passport.serializeUser(User.serializeUser());
 // This refers to how a user is got out of a session
 passport.deserializeUser(User.deserializeUser());
 
+// We have access to res.locals in our templates automatically
 // This middleware allows us to have access to req.flash on every single request
-// We have access to res.locals in our templates automatically.
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 });
-
-// app.use((req, res, next)=>{
-//     console.log(req.query);
-// });
-
-// Making a route where the creation of a new user is hardcoded
-// app.get('/fakeUser', async (req, res) => {
-//     const user = new User({email: '123@123.com', username: 'Rosa'});
-//     const newUser = await User.register(user, 'notagoodpassword');
-//     res.send(newUser);
-// })
 
 // A middleware that specifies what prefix is used for user routes
 app.use('/', userRoutes);
@@ -218,9 +195,9 @@ app.use('/campgrounds', campgroundRoutes);
 // A middleware that specifies what prefix to use for review routes
 app.use('/campgrounds/:id/reviews', reviewRoutes);
 
+
 app.get('/', (req, res) => {
     res.render('home')
-    // res.send('Hello from YelpCamp!')
 })
 
 app.all('*', (req, res, next) => {
@@ -238,25 +215,17 @@ app.use((err, req, res, next) => {
     // Error Object is an instance of the class ExpressError, 
     // that is an extended class of generic Express Error
 
-    // Give statusCode the default value
+    // Give statusCode and err.message the default values
     const {statusCode = 500} = err;
-
+    if (!err.message) err.message = 'Oh no, something went wrong!!!';
     
     // Intercept Mongoose 'CastError' in production mode and render HTTP 404 page
-    // For example, CastError happens when a user tries to view a campground that was deleted
-    // ( i.e. a user could make a bookmark for a campground in his browser before that campground was deleted)
+    // For example, CastError happens when a user tries to view a campground that was deleted ( i.e. a user could have saved a bookmark for a campground in his browser before that campground was deleted)
     if ((err.name==='CastError')&&(process.env.NODE_ENV==='production')){
-    // if ((err.name==='CastError')){
         return res.status(404).render('page404');
     }
 
-    if (!err.message){
-        err.message='Oh no, something went wrong!!!';
-    }
     res.status(statusCode).render('error', {err});
-
-    //  console.log(err.name);
-    // res.send('Something went wrong! :( ');
 })
 
 app.listen(3000, () => {
