6371bef2190ca385e530b76c7c178601f17f2e32
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2025-11-26 22:01:48 -0500

    Add functionality for Home Page for screen widths larger 768px to darken navbar if page is scrolled

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -2,10 +2,37 @@ const homePageNavToggler = document.querySelector('#homePageNavbarTogglerSvg');
 
 const homePageNav = document.querySelector('#homePageNavbar');
 
+const homePageBrand = document.querySelector('#homePageBrand');
+
 function toggleHomePageNav(){
     if(homePageNav){
         homePageNav.classList.toggle('showNavbar');
     }
 }
+
+function darkenNavbar() {
+    const screenWidth = window.innerWidth;
+    const scrollPosition = window.scrollY;
+    
+    if (screenWidth >= 768){
+        if (scrollPosition >= 48){
+            if(!homePageNav.classList.contains('navbarDark')){
+                homePageNav.classList.add('navbarDark');
+            }
+            if(!homePageBrand.classList.contains('navbarDark')){
+                homePageBrand.classList.add('navbarDark');
+            }
+        } else {
+            if(homePageNav.classList.contains('navbarDark')){
+                homePageNav.classList.remove('navbarDark');
+            }
+            if(homePageBrand.classList.contains('navbarDark')){
+                homePageBrand.classList.remove('navbarDark');
+            }
+        }
+    }
+}
+
+window.addEventListener('scroll', darkenNavbar);
 
 homePageNavToggler.addEventListener('click', toggleHomePageNav);
