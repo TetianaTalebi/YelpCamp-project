4815e4b5e3b2b9922974987da66f21a266a7be7c
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2025-12-02 13:19:45 -0500

    Add the following arrays into seeds/seedHelpers.js file: photos, authorsExternalDB and authorsLocalDB

--- seeds/seedHelpers.js
+++ seeds/seedHelpers.js
@@ -1,44 +1,289 @@
 module.exports.descriptors = [
     'Forest',
-    'Ancient',
-    'Petrified',
-    'Roaring',
-    'Cascade',
-    'Tumbling',
     'Silent',
-    'Redwood',
-    'Bullfrog',
     'Maple',
     'Misty',
     'Elk',
+    'Moose',
+    'Polar Bear',
     'Grizzly',
     'Ocean',
     'Sea',
+    'Birch',
     'Sky',
-    'Dusty',
-    'Diamond'
-]
+    'Spring',
+    'Winter',
+    'Summer',
+    'Cold',
+    'Icy',
+    'Frosty',
+    'Snowy',
+    'Blizzardy',
+    'Northern',
+];
 
 module.exports.places = [
     'Flats',
     'Village',
     'Canyon',
     'Pond',
+    'Mountains',
+    'Hill',
     'Group Camp',
-    'Horse Camp',
-    'Ghost Town',
     'Camp',
-    'Dispersed Camp',
     'Backcountry',
     'River',
     'Creek',
     'Creekside',
     'Bay',
-    'Spring',
     'Bayshore',
     'Sands',
-    'Mule Camp',
-    'Hunting Camp',
     'Cliffs',
-    'Hollow'
-]
+    'Lake',
+    'Waterfall',
+    'Northern Lights Camp',
+];
+
+module.exports.photos = [
+    [],
+    [
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638643/3de9c173-3ea6-4b3e-aecf-4624fd27a682_wg4z23.png',
+            filename: '3de9c173-3ea6-4b3e-aecf-4624fd27a682_wg4z23.png'
+        },{
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638642/47f38667-035f-407b-a764-8468e6f8458b_bgxrjx.png',
+            filename: '47f38667-035f-407b-a764-8468e6f8458b_bgxrjx.png'
+        },{
+            url:'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638643/813076d4-ab11-4ed8-a5d0-e4f24eca56c9_eblclu.png',
+            filename: '813076d4-ab11-4ed8-a5d0-e4f24eca56c9_eblclu.png'
+        },{
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638643/b886775b-3805-4539-b4bb-0f77c06ac19a_y6vxdi.png',
+            filename: 'b886775b-3805-4539-b4bb-0f77c06ac19a_y6vxdi.png'
+        }
+    ],
+    [
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638637/294dbc1d-9db7-49c3-926d-b2f061de2039_y4rvve.png',
+            filename: '294dbc1d-9db7-49c3-926d-b2f061de2039_y4rvve.png'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638642/8bbdcd34-17f1-47a3-b5cb-bffa29b03dd0_fmxoz1.png',
+            filename: '8bbdcd34-17f1-47a3-b5cb-bffa29b03dd0_fmxoz1.png'
+        }
+    ],
+    [],
+    [
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638637/98e0bc92-8ec3-40bb-a9f5-e9407b159084_jzx8me.webp',
+            filename: '98e0bc92-8ec3-40bb-a9f5-e9407b159084_jzx8me.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638637/c7b9b54d-678a-4ccb-b78e-c7f0ced7f9cd_ytz66z.webp',
+            filename: 'c7b9b54d-678a-4ccb-b78e-c7f0ced7f9cd_ytz66z.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638636/95019ba1-1df2-474d-b31e-17b5e5278525_gdazev.webp',
+            filename: '95019ba1-1df2-474d-b31e-17b5e5278525_gdazev.webp'
+        }
+    ],
+    [
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638641/058dab70-4f42-4b65-9829-5ffbf6ae90e0_dwgxoe.png',
+            filename: '058dab70-4f42-4b65-9829-5ffbf6ae90e0_dwgxoe.png'
+        }
+    ],
+    [],
+    [
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638635/68020929-7439-4414-9132-efccc6e47f00_rhdlfk.webp',
+            filename: '68020929-7439-4414-9132-efccc6e47f00_rhdlfk.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638636/a6333689-fbc1-4656-b1c5-42ccf5ac328a_ovum6a.webp',
+            filename: 'a6333689-fbc1-4656-b1c5-42ccf5ac328a_ovum6a.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638635/41b0e01b-6f04-4b99-a44d-8ce26fa330c5_vkwtpr.webp',
+            filename: '41b0e01b-6f04-4b99-a44d-8ce26fa330c5_vkwtpr.webp'
+        }
+    ],
+    [
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638632/2de1fdaa-b6e6-4c4b-85a7-ca53aa597c74_nmo6ps.webp',
+            filename: '2de1fdaa-b6e6-4c4b-85a7-ca53aa597c74_nmo6ps.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638634/b34af2f2-a799-431e-8d98-a42d42b721ce_xffwqp.webp',
+            filename: 'b34af2f2-a799-431e-8d98-a42d42b721ce_xffwqp.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638633/c2bd1163-535d-46f1-9add-fef1689d6fb3_zkeatu.webp',
+            filename: 'c2bd1163-535d-46f1-9add-fef1689d6fb3_zkeatu.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638634/6fef8337-555a-402b-be33-c09ea978793a_waudpg.webp',
+            filename: '6fef8337-555a-402b-be33-c09ea978793a_waudpg.webp'
+        }
+    ],
+    [
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638631/e902896d-4dba-484d-b5e2-89310cbdc669_a03rsl.webp',
+            filename: 'e902896d-4dba-484d-b5e2-89310cbdc669_a03rsl.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638630/62eb3fa3-ae95-412e-9797-3a2ddcfbbee1_omfprj.webp',
+            filename: '62eb3fa3-ae95-412e-9797-3a2ddcfbbee1_omfprj.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638631/288a1d9b-8d95-468e-ac7f-b348f2fe01e4_c2q1tn.webp',
+            filename: '288a1d9b-8d95-468e-ac7f-b348f2fe01e4_c2q1tn.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638631/9df84b9b-8005-465e-ac86-472a98961fef_cdlqxe.webp',
+            filename: '9df84b9b-8005-465e-ac86-472a98961fef_cdlqxe.webp'
+        }
+    ],
+    [
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638627/37542e1a-9388-41bc-bf3c-7a7248433feb_s1zmky.webp',
+            filename: '37542e1a-9388-41bc-bf3c-7a7248433feb_s1zmky.webp'
+        }
+    ],
+    [
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638629/ead1fa13-20c7-4002-92f3-1ee601f1bf5a_qvb2kv.webp',
+            filename: 'ead1fa13-20c7-4002-92f3-1ee601f1bf5a_qvb2kv.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638630/9c298239-613b-40a4-875f-bc435551d3fe_trfitz.webp',
+            filename: '9c298239-613b-40a4-875f-bc435551d3fe_trfitz.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638629/59e2f6a8-9f94-4a8b-b107-492d00f959a1_bzjzb6.webp',
+            filename: '59e2f6a8-9f94-4a8b-b107-492d00f959a1_bzjzb6.webp'
+        }
+    ],
+    [
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638629/b9101b90-054c-435d-a1c1-0ee7ae225d40_sodlru.webp',
+            filename: 'b9101b90-054c-435d-a1c1-0ee7ae225d40_sodlru.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638628/1609ac6c-9c55-4409-8d26-9854b97f1e39_gp6dvj.webp',
+            filename: '1609ac6c-9c55-4409-8d26-9854b97f1e39_gp6dvj.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638628/a26f004c-facc-4dce-9bf6-299b1cf5af1f_okk8mm.webp',
+            filename: 'a26f004c-facc-4dce-9bf6-299b1cf5af1f_okk8mm.webp'
+        }
+    ],
+    [
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638627/d9e6a73d-10bc-4c15-a8a4-c190954a0bc4_zygddb.webp',
+            filename: 'd9e6a73d-10bc-4c15-a8a4-c190954a0bc4_zygddb.webp'
+        }
+    ],
+    [
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638626/1e7f601a-57c8-4971-9217-0d2434d911ce_m2vn03.webp',
+            filename: '1e7f601a-57c8-4971-9217-0d2434d911ce_m2vn03.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638628/309f68de-d862-4817-b658-ba20475c9acd_vvrdd0.webp',
+            filename: '309f68de-d862-4817-b658-ba20475c9acd_vvrdd0.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638626/96723aff-e3d1-47dd-8b3d-e20c1577ec4a_xr9hfe.webp',
+            filename: '96723aff-e3d1-47dd-8b3d-e20c1577ec4a_xr9hfe.webp'
+        }
+    ],
+    [
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638626/207a514c-4cc1-415c-ae2e-4776e6b03911_wwqt33.webp',
+            filename: '207a514c-4cc1-415c-ae2e-4776e6b03911_wwqt33.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638625/2c699eeb-aa1d-4e50-b16d-add636558f60_xjo1sw.webp',
+            filename: '2c699eeb-aa1d-4e50-b16d-add636558f60_xjo1sw.webp'
+        }
+    ],
+    [
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638625/d8c4a069-1634-4907-b91f-57c80964ca72_ckhbzh.webp',
+            filename: 'd8c4a069-1634-4907-b91f-57c80964ca72_ckhbzh.webp'
+        }
+    ],
+    [
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638624/fc824ff6-6e54-4a08-a7e4-0d380b5d687f_ykohov.webp',
+            filename: 'fc824ff6-6e54-4a08-a7e4-0d380b5d687f_ykohov.webp'
+        }
+    ],
+    [
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638612/41bdb7f5-6751-43fe-b2eb-ad93e3b046a7_0_rekjii.png',
+            filename: '41bdb7f5-6751-43fe-b2eb-ad93e3b046a7_0_rekjii.png'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638612/67780f1f-1dd0-444f-acb9-851f8648b35b_3_fvawr5.png',
+            filename: '67780f1f-1dd0-444f-acb9-851f8648b35b_3_fvawr5.png'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638613/41bdb7f5-6751-43fe-b2eb-ad93e3b046a7_1_axh2qk.png',
+            filename: '41bdb7f5-6751-43fe-b2eb-ad93e3b046a7_1_axh2qk.png'
+        }
+    ],
+    [
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638617/86228d8d-97ec-4d8c-bd0b-b818083c0501_wyynna.webp',
+            filename: '86228d8d-97ec-4d8c-bd0b-b818083c0501_wyynna.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638617/f8f3bdec-f467-4ceb-9261-e6262d73e77b_c0hpij.webp',
+            filename: 'f8f3bdec-f467-4ceb-9261-e6262d73e77b_c0hpij.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638617/c5c2c2d1-319a-4e22-b5d6-91f8c7f93302_qnavcc.webp',
+            filename: 'c5c2c2d1-319a-4e22-b5d6-91f8c7f93302_qnavcc.webp'
+        }
+    ],
+    [
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638615/caa4b3f5-5f81-4974-8216-c2667865bfaa_rpzzim.webp',
+            filename: 'caa4b3f5-5f81-4974-8216-c2667865bfaa_rpzzim.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638623/ddd99240-2352-4195-9215-69ebc5365745_fbwxpf.webp',
+            filename: 'ddd99240-2352-4195-9215-69ebc5365745_fbwxpf.webp'
+        }
+    ],
+    [
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638622/4f36d52d-c923-45a5-8bed-a7aacf4a8f42_tuzwus.webp',
+            filename: '4f36d52d-c923-45a5-8bed-a7aacf4a8f42_tuzwus.webp'
+        },
+        {
+            url: 'https://res.cloudinary.com/dljmlxtmx/image/upload/v1764638622/8b3663cd-3b3d-4538-8b04-d0d64ea9297d_tarui3.webp',
+            filename: '8b3663cd-3b3d-4538-8b04-d0d64ea9297d_tarui3.webp'
+        }
+    ]
+];
+
+// Authors for seeding external database
+module.exports.authorsExternalDB = [
+    '69266abed79ae65ad49a161f',
+    '6927a89edb91a93bf8e8d14a',
+    '692e588fb1ef8eb59831f688',
+    '692e594ab1ef8eb59831f691',
+    '692e59bab1ef8eb59831f69a',
+    '692e5a1fb1ef8eb59831f6a3'
+];
+
+// Authors for seeding local database
+module.exports.authorsLocalDB = [
+    '66f0e0cfafa6e339ae846860',
+    '66f0e47a1397e51ecec74070',
+    '66f4636666984c9921b60753',
+    '66f5e674802d81bdfcf436cc'
+];
+
